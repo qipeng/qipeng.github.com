9c26ad0974d938f51e7234bb5f244ba64076223c
qipeng <pengrobertqi@163.com>
2014-04-10 21:16:00 -0700

    purecss style

--- media/js/main.js
+++ media/js/main.js
@@ -5,6 +5,5 @@ var email_name = {'qipeng':'qipeng.thu'}, email_domain = {'qipeng':'gmail.com'};
 function loadimg(){$("a.fancyimg").fancybox({'width': '75%','height': '75%','overlayShow': false,'transitionIn': 'fade','transitionOut': 'fade','titlePosition': 'inside','overlayColor': '#000','overlayOpacity': 0.9});
 $("a.fancyimg img").after('<div style="display:block;width:16px;height:16px;position:absolute;top:0px;right:0px;background:url(\'/media/images/fancycorner.png\') no-repeat;_background:none;_filter:progid:DXImageTransform.Microsoft.AlphaImageLoader(enabled=true, sizingMethod=noscale, src=\'/media/images/fancycorner.png\');"> </div>');
 $("a.fancyimg").wrap("<div style='width:auto;overflow:hidden;' />");}
-function loadgoogletranslate() {$("#googletranslate").html("<img src=\"/media/images/loader.gif\" />&nbsp;Loading Google Translate Gadget...");$.ajax({type: "GET",url: "/media/googletranslate.html",dataType:"html",success: function(data){$("#googletranslate").html(data);}});}
 function loadsociallinks() {$("#sociallinks").html("<img src=\"/media/images/loader.gif\" />&nbsp;Loading SNS Buttons...");$.ajax({type: "GET",url: "/media/sociallinks.html",dataType:"html",success: function(data){$("#sociallinks").html(data);}});}
 function sendEmail(name) {window.location.href="mailto:" + email_name[name] + "@" + email_domain[name];}
