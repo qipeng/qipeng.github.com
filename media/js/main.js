2bfca18a8bb505755e15fac066bde5140f599cb4
qipeng <pengrobertqi@163.com>
2014-11-03 10:12:01 -0800

    one-column responsive

--- media/js/main.js
+++ media/js/main.js
@@ -1,6 +1,6 @@
 // JavaScript Document
 
-var email_name = {'qipeng':'qipeng.thu'}, email_domain = {'qipeng':'gmail.com'};
+var email_name = {'pengqi':'pengqi'}, email_domain = {'pengqi':'cs.stanford.edu'};
 
 function loadimg(){$("a.fancyimg").fancybox({'width': '75%','height': '75%','overlayShow': false,'transitionIn': 'fade','transitionOut': 'fade','titlePosition': 'inside','overlayColor': '#000','overlayOpacity': 0.9});
 $("a.fancyimg img").after('<div style="display:block;width:16px;height:16px;position:absolute;top:0px;right:0px;background:url(\'/media/images/fancycorner.png\') no-repeat;_background:none;_filter:progid:DXImageTransform.Microsoft.AlphaImageLoader(enabled=true, sizingMethod=noscale, src=\'/media/images/fancycorner.png\');"> </div>');
